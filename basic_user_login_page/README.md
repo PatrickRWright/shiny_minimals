5041748c2818d2dde61dcef02706e472d8eacd94
Patrick Wright <patrick.wright@iomedico.com>
2021-08-20 11:43:25 +0000

    add login page timeout and externalise timeout and credentials

--- basic_user_login_page/README.md
+++ basic_user_login_page/README.md
@@ -2,8 +2,7 @@
 
 Example how the wrap a `shinydashboard` into a `shinymanager` login page.
 Credentials are hashed using the `scrypt` library. Timout before login is
-not implemented in this example. Without it, the session will not end while
-the user idles on the login page which can use up a lot of resources. A fix
-for this is discussed in the StackOverflow reference below.
+implemented in this example. Without it, the session will not end while
+the user idles on the login page which can use up a lot of resources.
 
 StackOverflow [reference](https://stackoverflow.com/questions/28987622/starting-shiny-app-after-password-input)
--- basic_user_login_page/app.R
+++ basic_user_login_page/app.R
@@ -2,24 +2,11 @@ library(shiny)
 library(shinydashboard)
 library(shinymanager)
 library(scrypt)
+library(purrr) # for map()
 
-# define credentials
-credentials <- data.frame(
-  user = c("shinyuser", "shinymanager"), # mandatory
-  # hashed passwords returned by scrypt::hashPassword()
-  # plain, unhashed pws in this case are "user123" and "manager123" 
-  # checking what the plain passwords are can be done with scrypt::verifyPassword()
-  password =  # mandatory
-    c("c2NyeXB0AA8AAAAIAAAAAVcQ/XDtSx6UUp0WQdbUF3jl6MMOyaTzDIiD+WBLlqWiy5Oh/5GmPH7pzNUMhfNhSG7hcbEUvjBAb+qWWFIU2+InkFsKFLDQTLkwnHQf1/xZ",
-      "c2NyeXB0ABEAAAAIAAAAAVkEDuuDT1rcwA4uCD1BQ0I+JqL6x3ImLJHNQ2hOMoyTOmUhHGZgmbjZDJNoIY3oI+suqmU2RkB/jPZ69dz7EVrvxY0p5o85wNf/G7hl3CjN"),
-  is_hashed_password = TRUE, # mandatory if hased passwords are provided
-  start = c("2019-04-15"), # optional (all others)
-  expire = c(NA, "2025-12-31"),
-  admin = c(FALSE, TRUE),
-  comment = c("regular user account", "admin account"),
-  stringsAsFactors = FALSE
-)
-
+# get externalised components
+map(list.files("basic_user_login_page/R", full.names = TRUE),
+    ~source(.x, encoding = "UTF-8"))
 
 ui <- dashboardPage(
   dashboardHeader(title = "A dashboard"),
@@ -36,8 +23,8 @@ ui <- dashboardPage(
   dashboardBody()
 )
 
-# Wrap your UI with secure_app
-ui <- shinymanager::secure_app(ui)
+# Wrap your UI with secure_app - head_auth enables the timeout on the login page
+ui <- shinymanager::secure_app(head_auth = tags$script(inactivity), ui)
 
 server <- function(input, output, session) {
 
