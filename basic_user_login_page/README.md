65033625a2b5fee9ea34f3cea62d1b619fda8ec9
Patrick Wright <patrick.wright@iomedico.com>
2021-08-20 11:12:50 +0000

    add credential hashing via scrypt library

--- basic_user_login_page/README.md
+++ basic_user_login_page/README.md
@@ -1,4 +1,9 @@
 # Basic user login page
 
 Example how the wrap a `shinydashboard` into a `shinymanager` login page.
-Credentials are hardcoded in the the app code in this example.
+Credentials are hashed using the `scrypt` library. Timout before login is
+not implemented in this example. Without it, the session will not end while
+the user idles on the login page which can use up a lot of resources. A fix
+for this is discussed in the StackOverflow reference below.
+
+StackOverflow [reference](https://stackoverflow.com/questions/28987622/starting-shiny-app-after-password-input)
--- basic_user_login_page/app.R
+++ basic_user_login_page/app.R
@@ -1,11 +1,18 @@
 library(shiny)
 library(shinydashboard)
 library(shinymanager)
+library(scrypt)
 
 # define credentials
 credentials <- data.frame(
   user = c("shinyuser", "shinymanager"), # mandatory
-  password = c("user123", "manager123"), # mandatory
+  # hashed passwords returned by scrypt::hashPassword()
+  # plain, unhashed pws in this case are "user123" and "manager123" 
+  # checking what the plain passwords are can be done with scrypt::verifyPassword()
+  password =  # mandatory
+    c("c2NyeXB0AA8AAAAIAAAAAVcQ/XDtSx6UUp0WQdbUF3jl6MMOyaTzDIiD+WBLlqWiy5Oh/5GmPH7pzNUMhfNhSG7hcbEUvjBAb+qWWFIU2+InkFsKFLDQTLkwnHQf1/xZ",
+      "c2NyeXB0ABEAAAAIAAAAAVkEDuuDT1rcwA4uCD1BQ0I+JqL6x3ImLJHNQ2hOMoyTOmUhHGZgmbjZDJNoIY3oI+suqmU2RkB/jPZ69dz7EVrvxY0p5o85wNf/G7hl3CjN"),
+  is_hashed_password = TRUE, # mandatory if hased passwords are provided
   start = c("2019-04-15"), # optional (all others)
   expire = c(NA, "2025-12-31"),
   admin = c(FALSE, TRUE),
